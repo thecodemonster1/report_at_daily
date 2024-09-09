c2fe8388ed77691581d4f28e8a54125aa55e1586
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2024-09-09 11:51:53 +0530

    update  view and settings pages

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -5,6 +5,8 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Center(
+      child: Text("This is Settings page..."),
+    );
   }
 }
---
+++ lib/pages/view_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ViewPage extends StatelessWidget {
+  const ViewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text("This is View page"),
+    );
+  }
+}
